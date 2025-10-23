70abd8a567f6c3a0022b1cf0fd1bf1e829752a35
cbanupama <cbanupama03@gmail.com>
2025-10-24 01:06:25 +0530

    Add 1.py - first Python program

---
+++ 1.py
@@ -0,0 +1,9 @@
+print("Hello Python!")
+
+print("Namaste, nanna hesaru Anupama!")
+
+name = "Manu"
+print(f"Namaste, nanna hesaru {name}!")
+
+name = input("Nimma hesaru yenu? ")
+print(f"Namaste, nanna hesaru {name}!")
